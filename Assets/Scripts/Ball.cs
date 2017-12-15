7e81d0305b59bef0bd69e3ae0a2683028e881b6f
Mário Gil <mg_mesquita@hotmail.com>
2017-12-15 16:34:22 +0000

    Initial file upload
    Currently doing the pin replacement code and animation

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Ball))]
+public class Ball : MonoBehaviour {
+
+	public float speedFactor;
+
+	private Rigidbody ballBody;
+	private AudioSource ballSound;
+
+    public bool hasLaunched = false;
+
+	// Use this for initialization
+	void Start () {
+		ballBody = GetComponent<Rigidbody> ();
+		ballSound = GetComponent<AudioSource> ();
+        ballBody.useGravity = false;
+	}
+
+	public void Launch (Vector3 velocity)
+	{
+        if (!hasLaunched) {
+            ballBody.velocity = velocity; // * speedFactor;
+            ballSound.Play();
+            ballBody.useGravity = true;
+            hasLaunched = true;
+        }
+	}
+
+    private void Update()
+    {
+        if (this.transform.position.y < 10.85)
+        {
+            this.transform.position = new Vector3(transform.position.x, 10.86f, transform.position.z);
+        }
+    }
+
+}
