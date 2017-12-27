242c9f97fa3b7109615d9c1348a4311976691043
Mário Gil <mg_mesquita@hotmail.com>
2017-12-27 23:20:01 +0000

    Test driven development
    Learned basic TDD principles.
    Started applying tests to code the scoring system behaviour.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent (typeof(Ball))]
 public class Ball : MonoBehaviour {
 
 	public float speedFactor;
---
+++ Assets/Scripts/BallDragLaunch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Ball))]
+public class BallDragLaunch : MonoBehaviour {
+
+    private Ball ball;
+    private Vector3 dragStart, dragEnd;
+    private float startTime, endTime;
+
+
+    void Start () {
+        ball = GetComponent<Ball>();
+	}
+
+    private void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -42.15f, 42.15f), transform.position.y, transform.position.z);
+    }
+    public void DragStart() { //
+        //capture time and position of drag start
+
+        dragStart = Input.mousePosition;
+        startTime = Time.time;
+    }
+
+    public void DragEnd() {
+        //launch ball
+        endTime = Time.time;
+        float timeInterval = endTime - startTime;
+
+        dragEnd = Input.mousePosition;
+        Vector3 positionVar = dragEnd - dragStart;
+        float launchSpeedX = (dragEnd.x - dragStart.x) / timeInterval;
+        float launchSpeedZ = (dragEnd.y - dragStart.y) / timeInterval;
+
+        Vector3 velocity = new Vector3 (launchSpeedX, 0, launchSpeedZ);
+        ball.Launch(velocity);
+        ball.hasLaunched = true;
+    }
+
+    public void MoveStart(float xNudge)
+    {
+        if (!ball.hasLaunched)
+        {
+            Vector3 translation = new Vector3(xNudge, 0f, 0f);
+            this.transform.position += translation;
+        }
+    }
+}
