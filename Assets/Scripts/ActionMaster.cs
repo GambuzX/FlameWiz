21e8d3fdb062528d82f17ffaa716a802d20cb5a2
Mário Gil <mg_mesquita@hotmail.com>
2017-12-28 22:05:45 +0000

    More TDD
    Can now recognize whether it is the first or the second bowl

--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -6,14 +6,40 @@ public class ActionMaster {
 
 	public enum Action {Tidy, Reset, EndTurn, EndGame};
 
+	//private int[] bowls = new int[21]; //array used to store bowls
+	public int bowl = 1;  //bowl number
+
 	public Action Bowl (int pins) {
 		if (pins < 0 || pins > 10) {throw new UnityException("Invalid number of pins");}
 
-		if (pins == 10) {
+		// If last bowl (23rd)
+		// Returns Action.EndTurn to complete score;
+
+		if (bowl == 23) {
+			bowl++;
 			return Action.EndTurn;
 		}
 
-		//Other behaviour here
+		// If after last bowl (23rd)
+		// Returns Action.EndGame;
+
+		if (bowl > 23) {
+			return Action.EndGame;
+		}
+
+		if (pins == 10) {
+			bowl += 2;
+			return Action.EndTurn;
+		}
+
+		// If first bowl of frame
+		// Return Action.Tidy;
+		if (bowl % 2 != 0) { // Mid frame (or last frame)
+			bowl++;
+			return Action.Tidy;
+		} else if (bowl % 2 == 0) { // If second bowl of frame Return Action.EndTurn;
+			return Action.EndTurn;
+		}
 
 		throw new UnityException ("Not sure what action to return");
 	}
---
+++ Assets/Scripts/ActionMasterTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class ActionMasterTest {
+
+	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+	private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+	private ActionMaster.Action endGame = ActionMaster.Action.EndGame;
+
+	private ActionMaster actionMaster;
+
+	[SetUp] // Setups an actionMaster
+	public void Setup(){
+		actionMaster = new ActionMaster();
+	}
+
+	[Test]
+	public void T00PassingTest() {
+		Assert.AreEqual (1, 1);
+	}
+
+	[Test]
+	public void T01OneStrikeReturnsEndTurn() {
+		Assert.AreEqual (endTurn, actionMaster.Bowl(10));
+	}
+
+	[Test]
+	public void T02Bowl8ReturnsTidy() {
+		Assert.AreEqual (tidy, actionMaster.Bowl(8));
+	}
+
+	[Test]
+	public void T03Bowl28SpareReturnsEndTurn() {
+		actionMaster.Bowl (2);
+		Assert.AreEqual (endTurn, actionMaster.Bowl(8));
+	}
+
+	[Test]
+	public void T04SecondBowlReturnsEndTurn() {
+		actionMaster.bowl = 2;
+		Assert.AreEqual (endTurn, actionMaster.Bowl(5));
+	}
+
+	[Test]
+	public void T05LastBowlReturnsEndTurn() {
+		actionMaster.bowl = 23;
+		Assert.AreEqual (endTurn, actionMaster.Bowl(7));
+	}
+
+	[Test]
+	public void T06AfterLastBowlEndsGame() {
+		actionMaster.bowl = 24;
+		Assert.AreEqual (endGame, actionMaster.Bowl(8));
+	}
+
+}
+
