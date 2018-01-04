5e65ddbbd9c50efbc0d8923c3b5ed6a0504102b2
Mário Gil <mg_mesquita@hotmail.com>
2018-01-04 21:30:48 +0000

    Score Master Setup

---
+++ Assets/Scripts/ScoreMaster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreMaster {
+    
+    public static List<int> ScoreFrames (List<int> rolls)
+    {
+        List<int> frameList = new List<int>();
+
+        //Code here
+        //Do this using TDD
+        //Returns scores for each frame
+        return frameList;
+    }
+}
