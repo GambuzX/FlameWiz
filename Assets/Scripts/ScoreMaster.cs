78df9ad404a90eed77c5676bdbac3bb42f0a61bd
Mário Gil <mg_mesquita@hotmail.com>
2018-01-15 19:50:20 +0000

    Continued with ScoreMaster
    Only missing the last set special cases.

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -13,23 +13,84 @@ public class ScoreMaster {
         //Do this using TDD
         //Returns scores for each frame
 
-        //Rules -> Strike: Adds to frame the score of the next frame (max 2 frames forward)
+        //Rules -> Strike: Adds to frame the score of the next 2 bowls
         //      -> Spare: Adds to frame the score of the first bowl in next frame 
 
         int sum = 0;
         bool secondFrame = false;
-        bool strike = false;
         int previousRoll = 0;
         int strikeCumulative = 0;
 
-        foreach (int roll in rolls) {
+        int rollsLength = rolls.Count;
+        bool firstBowlOfSet = true;
+        
+        for (int i = 0; i < rollsLength; i++)
+        {
+            if (firstBowlOfSet)
+            {
+                int roll = rolls[i];
+                int nextRoll = 0;
+                int nextnextRoll = 0;
+                bool nextRollValid = true;
+                bool nextnextRollValid = true;
+
+                if (i < (rollsLength - 2))
+                {
+                    nextRoll = rolls[i + 1];
+                    nextnextRoll = rolls[i + 2];
+                    nextRollValid = true;
+                    nextnextRollValid = true;
+                }
+                else if (i < (rollsLength - 1))
+                {
+                    nextRoll = rolls[i + 1];
+                    nextnextRollValid = false;
+                    nextRollValid = true;
+                }
+                else
+                {
+                    nextRollValid = false;
+                    nextnextRollValid = false;
+                }
+
+                if (roll == 10) //if strike
+                {
+                    if (nextRollValid && nextnextRollValid)
+                    {
+                        frameList.Add(10 + nextRoll + rolls[i + 2]);
+                    }
+                }
+                else if ((roll + nextRoll) == 10) // if spare on set
+                {
+                    if (nextnextRollValid)
+                    {
+                        frameList.Add(10 + rolls[i + 2]);
+                        firstBowlOfSet = false;
+                    }
+                }
+                else
+                {
+                    if (nextRollValid)
+                    {
+                        frameList.Add(roll + nextRoll);
+                        firstBowlOfSet = false;
+                    }
+                }
+            }
+            else //if second bowl of set
+            {
+                firstBowlOfSet = true;
+            }
+        }
+
+        /*foreach (int roll in rolls) {
             if (secondFrame) {
                 sum += roll;
 
-                if (strike) { //if last frame was a strike
+                if (strikeCumulative == 10) { //if last frame was a strike
                     int value = strikeCumulative + sum;
                     frameList.Add(value);
-                    strike = false;
+                    strikeCumulative = 0;
                 }
 
                 if (sum >= 10) //spare
@@ -44,12 +105,25 @@ public class ScoreMaster {
                 secondFrame = false;
             }
             else { //first frame           
+
                 if (roll == 10) //if strike
                 {
-                    strike = true;
-                    strikeCumulative += 10;
+                    if (strikeCumulative >= 20)//if last two frames were strike
+                    {
+                        frameList.Add(30); //adds 30
+                    } else
+                    {
+                        strikeCumulative += 10;
+                        rolls.
+                    }
                 }
-                else {
+                else { //not a strike
+                    if (strikeCumulative == 20) //if last two frames were strike and this bowl isnt
+                    {
+                        int score = 20 + roll;
+                        frameList.Add(score);
+                        strikeCumulative = 0;
+                    }
                     sum += roll;
                     secondFrame = true;
                     if (previousRoll == 10) //if last frame scored 10 pins
@@ -60,8 +134,7 @@ public class ScoreMaster {
                     }
                 }
             }
-        }
-
+        }*/
         return frameList;
     }
 
--- Assets/Scripts/ScoreMasterTest.cs
+++ Assets/Scripts/ScoreMasterTest.cs
@@ -109,7 +109,7 @@ public class ScoreMasterTest {
 		int[] frames = {22, 15,   5};
 		Assert.AreEqual (frames.ToList(), ScoreMaster.ScoreFrames (rolls.ToList()));
 	}
-/*
+
 	[Test]
 	public void T15MultiStrikes3 () {
 		int[] rolls = { 10, 10, 2,3, 10, 5,3};
@@ -151,14 +151,14 @@ public class ScoreMasterTest {
 		int[] totalS = {  2,   4,   6,   8,  10,  12,  14,  16,  18,    35};
 		Assert.AreEqual (totalS.ToList(), ScoreMaster.ScoreCumulative (rolls.ToList()));
 	}
-	
+
 	[Test]
 	public void T21StrikeInLastFrame () {
 		int[] rolls = { 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 10,2,3};
 		int[] totalS = {  2,   4,   6,   8,  10,  12,  14,  16,  18,     33};
 		Assert.AreEqual (totalS.ToList(), ScoreMaster.ScoreCumulative (rolls.ToList()));
 	}
-
+/*	
 	// http://slocums.homestead.com/gamescore.html
 	[Test]
 	[Category ("Verification")]
